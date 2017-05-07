e6faa1738c92f8fe854f39f5100e017d82aff548
Andrey Vokin <andrey.vokin@gmail.com>
2017-05-08 01:23:45 +0200

    Tasks started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
+import {TaskService} from './task.service';
+import {AppRoutingModule} from './app-routing.module';
+import {TasksComponent} from './tasks.component';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [ AppComponent, TasksComponent ],
+  providers:    [ TaskService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

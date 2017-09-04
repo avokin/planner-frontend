6a205c382fdb260e9025e64aaaba9698986801df
Andrey Vokin <andrey.vokin@gmail.com>
2017-09-04 21:57:30 +0200

    Fixed calendar date

--- src/app/calendar.component.ts
+++ src/app/calendar.component.ts
@@ -25,7 +25,7 @@ export class CalendarComponent implements OnInit {
 
   getCalendarUrl(): SafeResourceUrl {
     if (this.cashedUrl == null) {
-      this.cashedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://calendar.google.com/calendar/embed?showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=DAY&wkst=1&bgcolor=%23FFFFFF&src=andrey.vokin%40gmail.com&color=%231B887A&ctz=Europe%2FBerlin&dates=20170904/20170904`);
+      this.cashedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://calendar.google.com/calendar/embed?showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=DAY&wkst=1&bgcolor=%23FFFFFF&src=andrey.vokin%40gmail.com&color=%231B887A&ctz=Europe%2FBerlin&dates=${this.dayId}/${this.dayId}`);
     }
     return this.cashedUrl;
   }
